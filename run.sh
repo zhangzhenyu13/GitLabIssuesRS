e5f8fa2520ff84f20ecbb14d7f8e4a4c823654cd
Bird Zhang <zhangzhenyu13@outlook.com>
2018-05-10 17:07:27 +0800

    sync

---
+++ run.sh
@@ -0,0 +1,6 @@
+python RunningModel.py -i 2003426 -p 8001 >> ../LOGDATA/log2003426.log &
+python RunningModel.py -i 2003426 -p 8002 >>../LOGDATA/log2003426.log &
+python RunningModel.py -i 4180516 -p 8003 >>../LOGDATA/log4180516.log &
+python RunningModel.py -i 950246 -p 8004 >>../LOGDATA/log950246.log &
+python RunningModel.py -i 3836952 -p 8005 >>../LOGDATA/log3836952.log &
+
