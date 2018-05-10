27f74c33652d4827eb786f2d6a9ef6a031a9c5c1
bird <zhangzhenyu13@outlook.com>
2018-05-10 17:05:28 +0800

    final test

---
+++ run.sh
@@ -0,0 +1,5 @@
+python RunningModel.py -i 2003426 -p 8001 >> ../log2003426.log &
+python RunningModel.py -i 2003426 -p 8002 >>../log2003426.log &
+python RunningModel.py -i 4180516 -p 8003 >>../log4180516.log &
+python RunningModel.py -i 950246 -p 8004 >>../log950246.log &
+python RunningModel.py -i 3836952 -p 8005 >>../log3836952.log &
